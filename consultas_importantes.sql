b2d9122cc6f55f1b5cece60ca01a7ccb49ebd408
alex <alejandrosilvarodriguez9@gmail.com>
2024-03-28 19:12:10 +0100

    añadido consultas importantes

---
+++ consultas_importantes.sql
@@ -0,0 +1,22 @@
+-- consulta para coger las enfermedades de una persona
+Select dni,descripcion
+from paciente p,paciente_enfermedad pe,enfermedad e
+where p.dni=pe.paciente_dni and pe.enfermedad_codigo_enfermedad=e.codigo_enfermedad and p.dni=95592093;
+
+-- consulta para coger las terapias de una persona 
+select p.dni,t.codigo_de_terapia,t.descripcion,t.duracion, pr.fecha_inicio
+from paciente p,prescripcion pr, terapia t
+where p.dni=pr.paciente_dni and pr.terapia_codigo_de_terapia=t.codigo_de_terapia and p.dni=95592093
+order by pr.fecha_inicio desc;
+
+-- consulta para coger las terapias de una persona validadas para que el paciente la pueda ver
+select p.dni,t.codigo_de_terapia,t.descripcion,t.duracion
+from paciente p,prescripcion pr, terapia t
+where p.dni=pr.paciente_dni and pr.terapia_codigo_de_terapia=t.codigo_de_terapia and pr.validacion=1 and p.dni=95592093;
+
+-- query para validar una prescripcion
+UPDATE prescripcion pr
+SET pr.validacion = 1
+WHERE pr.paciente_dni = '95592093'
+AND pr.medico_dni = '65112919'
+AND pr.terapia_codigo_de_terapia = 'EAGXSAZKBF';
